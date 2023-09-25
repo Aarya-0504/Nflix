4979de55eb27a14a78fc5793eb117ae61529d6b2
Aarya_P <aarya.patil@spit.ac.in>
2023-09-25 20:51:18 +0530

    login page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import Home from './pages/home/Home';
+import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
 
 
@@ -7,7 +8,8 @@ import Watch from './pages/watch/Watch';
 function App() {
   return (
     <div className='App'>
-      <Home/>
+      {/* <Home/> */}
+      <Register/>
       {/* <Watch/> */}
     </div>
   );
