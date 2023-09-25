1baa9554f7f6808528c4fdaffa1103e571e88f5a
Aarya_P <aarya.patil@spit.ac.in>
2023-09-25 19:52:20 +0530

    Watch Page Complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Home from './pages/home/Home';
+import Watch from './pages/watch/Watch';
 
 
 
 function App() {
   return (
     <div className='App'>
-      <Home/>
+      {/* <Home/> */}
+      <Watch/>
     </div>
   );
 }
