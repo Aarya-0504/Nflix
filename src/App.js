ccb7b01ad32a9ea73741f26b421a072dc8e2cf4b
Aarya_P <aarya.patil@spit.ac.in>
2023-09-25 21:49:44 +0530

    login complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <div className='App'>
       {/* <Home/> */}
-      <Register/>
+      {/* <Register/> */}
+      <Login/>
       {/* <Watch/> */}
     </div>
   );
